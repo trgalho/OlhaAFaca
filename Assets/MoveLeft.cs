e526f6587c069f3093e2b85dad0b37489f9371fe
Talisson Galho <trgalho@gmail.com>
2018-08-16 18:28:40 -0300

    Commit Retroativo 14-08-2018
    Commit retroativo (Arquivos do dia 14/08/2018)

---
+++ Assets/MoveLeft.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	 }
+	
+	// Update is called once per frame
+	void Update () {
+		(this.GetComponent("Rigidbody2D") as Rigidbody2D).AddForce(transform.up*5);
+		
+	}
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    private float speed;
+	// Use this for initialization
+	void Start () {
+        changeSpeed();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.deltaTime % 10 == 0.0f) changeSpeed();
+        transform.Rotate(Vector3.forward*speed);
+		
+	}
+    void changeSpeed() {
+        speed = Random.Range(0.1f,5);
+    }
+}
